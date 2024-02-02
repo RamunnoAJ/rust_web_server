40f92e5b9efc789262a3cc969c638420c4585c7c
RamunnoAJ <ramunnoagustin@gmail.com>
2024-02-02 13:29:58 -0300

    feat: send code from threadpool to a thread

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
-use std::thread;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+struct Job;
 
 #[derive(Debug)]
 pub struct PoolCreationError;
@@ -16,15 +22,17 @@ impl ThreadPool {
     ///
     /// The `new` function will panic if the size is zero.
     pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
         match size {
             0 => Err(PoolCreationError),
-            _ => Ok(ThreadPool { workers }),
+            _ => Ok(ThreadPool { workers, sender }),
         }
     }
 
@@ -41,8 +49,10 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
 
         Worker { id, thread }
     }
